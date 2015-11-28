bd2fffb59af250691b7e182a4f7fc4578a5956db
CSAR101 <arathore@smu.edu>
2015-11-28 01:58:02 -0600

    Adding previous work

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,75 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+@KBD
+D = A
+@pixelSup
+M = D
+
+@SCREEN
+D = A
+@pixel
+M = D
+
+(CLEAR_SCREEN)
+	@1
+	D = A
+	@pixel
+	M = M - D
+(CLEAR_SCREEN_LOOP)
+	@KBD
+	D = M
+	@BLACK_SCREEN
+	D;JGT
+	
+	@pixel
+	D = M
+	@1
+	D = D + A
+	@SCREEN
+	D = D - A
+	@CLEAR_SCREEN_LOOP
+	D;JEQ
+	
+	@pixel
+	A = M
+	M = 0
+	@1
+	D = A
+	@pixel
+	M = M - D
+	@CLEAR_SCREEN_LOOP
+	0;JMP
+
+(BLACK_SCREEN)
+	@1
+	D = A
+	@pixel
+	M = M + D
+(BLACK_SCREEN_LOOP)
+	@KBD
+	D = M
+	@CLEAR_SCREEN
+	D;JEQ
+	
+	@pixelSup
+	D = M
+	@pixel
+	D = D - M
+	@BLACK_SCREEN_LOOP
+	D;JEQ
+	
+	@pixel
+	A = M
+	M = -1
+	@1
+	D = A
+	@pixel
+	M = M + D
+	@BLACK_SCREEN_LOOP
+	0;JMP
+
+(EXIT)
+	@EXIT
+	0;JMP
