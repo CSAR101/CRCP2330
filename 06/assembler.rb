cfb6b255a37dfe03f9669c3afbb731d242a7b60c
CSAR101 <arathore@smu.edu>
2015-12-08 23:41:16 -0600

    Adding in logic for determining if files exist and end with .asm

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,15 +1,10 @@
-# # languageName = "ruby"
-
-# # puts "Hello #{languageName}, how are you?"
-
-# puts "Hi, what is your name?"
-
-# name = gets
-
-# puts "Hello #{name}, how are you?"
-
 #! /usr/bin/env ruby
 
-asm_filename = ARGV[0]
 
-puts asm_filename
+if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && 
+	File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
+  asm_filename = ARGV[0]
+  puts asm_filename
+else
+  abort("Usage: ./assembler.rb Prog.asm")
+end
