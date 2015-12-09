64433876769bdc6bf113942d095ea7f3ea2ca33f
CSAR101 <arathore@smu.edu>
2015-12-08 23:18:25 -0600

    Ruby get function like C++ cin for user input

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,3 +1,9 @@
-languageName = "ruby"
+# languageName = "ruby"
 
-puts "Hello #{languageName}, how are you?"
+# puts "Hello #{languageName}, how are you?"
+
+puts "Hi, what is your name?"
+
+name = gets
+
+puts "Hello #{name}, how are you?"
