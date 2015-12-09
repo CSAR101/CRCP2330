b8917494230dea6084eefaacee89a87892aca9a9
CSAR101 <arathore@smu.edu>
2015-12-08 23:29:26 -0600

    Lesson regarding Ruby ARGV and grabbing hold of files

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,9 +1,15 @@
-# languageName = "ruby"
+# # languageName = "ruby"
 
-# puts "Hello #{languageName}, how are you?"
+# # puts "Hello #{languageName}, how are you?"
 
-puts "Hi, what is your name?"
+# puts "Hi, what is your name?"
 
-name = gets
+# name = gets
 
-puts "Hello #{name}, how are you?"
+# puts "Hello #{name}, how are you?"
+
+#! /usr/bin/env ruby
+
+asm_filename = ARGV[0]
+
+puts asm_filename
