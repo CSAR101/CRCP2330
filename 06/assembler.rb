c37166cdec3e39142c337dac73e0d25c46ab2faf
Amit Rathore <arathore@smu.edu>
2015-12-10 17:07:55 -0600

    Refactoring the ruby code to make it more elegant and clean and improving logic

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,15 +1,21 @@
 #! /usr/bin/env ruby
 
-
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
-  asm_filename = ARGV[0]
-  puts asm_filename
-else
-  abort("Usage: ./assembler.rb Prog.asm")
+def args_valid?
+  ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
 end
 
-if (File.exist?(asm_filename) && File.readable?(asm_filename))
-  puts "The contents of #{asm_filename}"
-else
-  abort("#{asm_filename} not found or is unreadable")
+def is_readable?(path)
+  File.readable?(path)
+end
+
+unless args_valid?
+  abort("Usage: ./assembler.rb Prog.asm")
 end 
+
+asm_filename = ARGV[0]
+
+unless is_readable?(asm_filename)
+  abort("#{asm_filename} not found or is unreadable")
+end
+
+puts "The contents of #{asm_filename}"
