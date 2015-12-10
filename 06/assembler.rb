f792332d1c96891e28396e48e9f3bb16fa351d80
Amit Rathore <arathore@smu.edu>
2015-12-10 16:49:09 -0600

    Adding more logic to the assembler.rb code to determine if file is found or readable

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,10 +1,15 @@
 #! /usr/bin/env ruby
 
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && 
-	File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
+if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1)
   asm_filename = ARGV[0]
   puts asm_filename
 else
   abort("Usage: ./assembler.rb Prog.asm")
 end
+
+if (File.exist?(asm_filename) && File.readable?(asm_filename))
+  puts "The contents of #{asm_filename}"
+else
+  abort("#{asm_filename} not found or is unreadable")
+end 
